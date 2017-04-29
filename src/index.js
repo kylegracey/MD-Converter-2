4288579bf3406dea3bc7ea4b17f7dbe581f0d833
Kyle Gracey <kyle@kylegracey.com>
2017-04-29 13:42:32 -0500

    Read stream on server
    Wrapping my head around readStream. Working on an http server.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const http = require('http');
 const fs = require('fs');
 
-console.log('Script complete.');
+//Paths to local files
+const inputSrc = "./json/input.json";
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  let read_stream = fs.createReadStream(inputSrc);
+  read_stream.on('data', writeCallback);
+  read_stream.on('close', closeCallback);
+
+  function writeCallback(data) {
+    console.log(data);
+  }
+
+  function closeCallback() {
+    console.log('Stream closed.');
+    response.end();
+  }
+}).listen(3000, '127.0.0.1');
+
+console.log('Server started.');
